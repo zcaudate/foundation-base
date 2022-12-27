db7e0feec7fe6c5dd4eb72ba7f93f13082128ff7
Chris Zheng <z@caudate.me>
2022-12-27 13:10:14 +0800

    adding lua.torch tests

--- src/lua/torch/optim.clj
+++ src/lua/torch/optim.clj
@@ -8,23 +8,29 @@
   {:macro-only true
    :bundle  {:default [["optim" :as optim]]}})
 
+(def +optim+
+  ["rmsprop"
+   "cmaes"
+   "lbfgs"
+   "adadelta"
+   "FistaLS"
+   "polyinterp"
+   "adamax"
+   "adagrad"
+   "Logger"
+   "sgd"
+   "ConfusionMatrix"
+   "checkgrad"
+   "lswolfe"
+   "rprop"
+   "adam"
+   "cg"
+   "de"
+   "nag"
+   "asgd"])
 
-["rmsprop"
- "cmaes"
- "lbfgs"
- "adadelta"
- "FistaLS"
- "polyinterp"
- "adamax"
- "adagrad"
- "Logger"
- "sgd"
- "ConfusionMatrix"
- "checkgrad"
- "lswolfe"
- "rprop"
- "adam"
- "cg"
- "de"
- "nag"
- "asgd"]
+(h/template-entries [l/tmpl-entry {:type :fragment
+                                   :base "optim"
+                                   :tag "lua"
+                                   :shrink true}]
+  +optim+)

3dd4841cb4a8cb136637bc537b1cd4bb829d48f3
Chris Zheng <chris@statstrade.io>
2025-02-24 23:31:08 +1100

    fixing libpython tests

--- src/rt/libpython.clj
+++ src/rt/libpython.clj
@@ -5,8 +5,15 @@
             [rt.basic.impl.process-python :as python]
             [std.lang.base.impl :as impl]
             [std.lang.base.runtime :as default]
-            [std.lang.base.pointer :as ptr]
-            [libpython-clj2.python :as lp]))
+            [libpython-clj2.python :as lp]
+            [std.concurrent :as cc]
+            [std.lang :as l]
+            [std.lib :as h :refer [defimpl]]
+            [std.string :as str]
+            [xt.lang.base-repl :as k]))
+
+(defonce +preamble+
+  (delay (lp/initialize!)))
 
 (defn eval-raw
   "performs an exec expression
@@ -14,9 +21,9 @@
    (str (eval-raw +js+ \"1 + 1\"))
    => \"2\""
   {:added "3.0"}
-  ([ctx string]
-   (let [lang (raw-lang ctx)]
-     (.eval ^Context ctx (name lang) string))))
+  ([_ string]
+   @+preamble+
+   (lp/run-simple-string string)))
 
 (defn eval-libpython
   "evals body in the runtime
@@ -25,27 +32,12 @@
                     \"1+1\"))
    => \"2\""
   {:added "4.0"}
-  ([{:keys [raw]} string]
-   (eval-raw @raw string)))
+  ([_ string]
+   (get-in (eval-raw nil string)
+           [:globals "OUT"])))
 
 (def +options+
-  (delay
-    {:js      {:bootstrap (impl/emit-entry-deps
-                           k/return-eval
-                           {:lang :js
-                            :layout :flat})
-               :main  {:out   str}
-               :emit  {:body  {:transform #'default/return-transform}}}
-     :python  {:bootstrap  (impl/emit-entry-deps
-                            k/return-eval
-                            {:lang :python
-                             :layout :flat})
-               :main  {:out   (comp json/read str)}
-               :emit  {:body  {:transform (fn [forms mopts]
-                                            (default/return-transform
-                                             forms mopts {:format-fn identity
-                                                          :wrap-fn (fn [forms]
-                                                                     (apply list 'do forms))}))}}}}))
+  {})
 
 (defn invoke-libpython
   "invokes a pointer in the runtime
@@ -56,40 +48,28 @@
    => -1"
   {:added "4.0"}
   ([{:keys [state lang layout] :as rt} ptr args]
-   (let [{:keys [main emit]} (get @+options+ lang)]
-     (default/default-invoke-script
-      rt ptr args eval-libpython
-      {:main main
-       :emit emit
-       :json :full}))))
+   (default/default-invoke-script
+    rt ptr args eval-libpython
+    {:main  {}
+     :emit  {:body  {:transform #'python/default-body-transform}}})))
 
 (defn start-libpython
-  "starts the graal runtime"
+  "starts the libpython runtime"
   {:added "3.0"}
-  ([{:keys [lang resource system raw] :as graal}]
-   (if (nil? @raw)
-     (reset! raw (make-raw {:lang lang
-                            :resource resource
-                            :system system})))
-   (eval-libpython graal (get-in @+options+ [lang :bootstrap]))
-   graal))
+  [rt]
+  @+preamble+
+  rt)
 
 (defn stop-libpython
-  "stops the graal runtime"
+  "stops the libpython runtime"
   {:added "3.0"}
-  ([{:keys [lang raw] :as graal}]
-   (if-not (nil? @raw)
-     (h/swap-return! raw
-                     (fn [ctx]
-                       (close-raw ctx)
-                       [ctx nil])))
-   graal))
+  ([rt] rt))
 
 (defn- rt-libpython-string
-  ([graal]
-   (str "#rt.graal" (into {} graal))))
+  ([rt]
+   (str "#rt.libpython" (into {} rt))))
 
-(defimpl RuntimeGraal [lang raw]
+(defimpl RuntimeLibpython [lang raw]
   :string rt-libpython-string
   :protocols [std.protocol.component/IComponent
               :exclude [-kill]
@@ -100,7 +80,7 @@
                        -invoke-ptr invoke-libpython}])
 
 (defn rt-libpython:create
-  "creates a graal runtime
+  "creates a libpython runtime
  
    (h/-> (rt-libpython:create {:lang :js})
          (h/start)
@@ -110,22 +90,20 @@
   [{:keys [id]
     :or {id (h/sid)}
     :as m}]
-  (map->RuntimeGraal (assoc m
-                            :id id
-                            :raw (atom nil))))
+  (map->RuntimeLibpython (assoc m :id id)))
 
 (defn rt-libpython
-  "creates and starts a graal runtime"
+  "creates and starts a libpython runtime"
   {:added "3.0"}
   ([m]
    (-> (rt-libpython:create m)
        (h/start))))
 
 (defn rt-libpython?
-  "checks that object is a graal runtime"
+  "checks that object is a libpython runtime"
   {:added "3.0"}
   [obj]
-  (instance? RuntimeGraal obj))
+  (instance? RuntimeLibpython obj))
 
 ;;
 ;; Hooks for lang
@@ -133,12 +111,7 @@
 
 (def +init+
   [(default/install-type!
-    :python :graal
-    {:type :hara/rt.graal
+    :python :libpython
+    {:type :hara/rt.libpython
      :config {:lang :python :layout :full}
-     :instance {:create rt-libpython:create}})
-   (default/install-type!
-    :js :graal
-    {:type :hara/rt.graal
-     :config {:lang :js :layout :full}
      :instance {:create rt-libpython:create}})])
---
+++ test/rt/libpython_test.clj
@@ -0,0 +1,69 @@
+(ns rt.libpython-test
+  (:use code.test)
+  (:require [rt.libpython :as lp]
+            [std.lib :as h]
+            [std.lang :as l]
+            [std.concurrent :as cc]))
+
+(l/script- :python
+  {:runtime :libpython
+   :require [[xt.lang.base-lib :as k]]})
+
+(defn.py add10
+  [x]
+  (return (+ x 10)))
+
+^{:refer rt.libpython/TESTING :added "3.0"}
+(fact "performs an exec expression"
+  ^:hidden
+  
+  (add10 1)
+  => 11
+
+  (type (!.py
+          (var b 2)
+          -/add10))
+  => :pyobject
+  
+  ((!.py
+     (var b 2)
+     -/add10)
+   1)
+  => 11)
+
+^{:refer rt.libpython/eval-raw :added "3.0"}
+(fact "performs an exec expression"
+  ^:hidden
+  
+  (get-in (lp/eval-raw nil "OUT = 1 + 1")
+          [:globals "OUT"])
+  => 2)
+
+^{:refer rt.libpython/eval-libpython :added "4.0"}
+(fact "evals body in the runtime"
+  
+  (lp/eval-libpython nil
+                     "OUT = 1 + 1")
+  => 2)
+
+^{:refer rt.libpython/invoke-libpython :added "4.0"}
+(fact "invokes a pointer in the runtime"
+
+  (lp/invoke-libpython (l/rt :python)
+                       k/sub
+                       [1 2])
+  => -1)
+
+^{:refer rt.libpython/start-libpython :added "3.0"}
+(fact "starts the libpython runtime")
+
+^{:refer rt.libpython/stop-libpython :added "3.0"}
+(fact "stops the libpython runtime")
+
+^{:refer rt.libpython/rt-libpython:create :added "4.0"}
+(fact "creates a libpython runtime"
+
+  (h/-> (rt-libpython:create {:lang :js})
+        (h/start)
+        (h/stop))
+  => rt-libpython?)
